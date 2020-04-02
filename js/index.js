3afacc61afe4983f67f9cf41ac410b22c8e0c5cb
Cesar Morales <cesarlenin2@live.com>
2020-04-02 14:36:32 -0700

    finished nav

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
 'use strict';
 
-function myFunction() {
-  var x = document.getElementById('myLinks');
-  if (x.className === 'links') {
-    x.className += ' open';
-  } else {
-    x.className = 'links';
-  }
+
+
+function handleNav(){
+  $('nav').on('click','.icon', function (event){
+    event.preventDefault();
+    var x = document.getElementById('myLinks');
+    if (x.className === 'links') {
+      x.className += ' open';
+    } else {
+      x.className = 'links';
+    }
+  });
 }
+
+function render(){
+  $('main').html(choiceTemplate());
+}
+
+function handlePorfolio () {
+  handleNav();
+  render();
+}
+
+$(handlePorfolio);
